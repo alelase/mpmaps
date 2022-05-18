f44dddb8e9789f5e9bb9f90af0d34999690fa794
alejandrol <alejandrol@mprest.com>
2022-05-18 11:33:47 +0300

    add doubleclick callback on ClusterMarker

--- dist/components/ClusterMarker/ClusterMarker.d.ts
+++ dist/components/ClusterMarker/ClusterMarker.d.ts
@@ -5,6 +5,7 @@ interface ClusterMarkerOptions extends google.maps.MarkerOptions {
     position: google.maps.LatLngLiteral;
     count: number;
     onClick: () => void;
+    onDoubleClick?: () => void;
 }
 declare const ClusterMarker: React.FC<ClusterMarkerOptions>;
 export default ClusterMarker;
--- dist/components/ClusterMarker/ClusterMarker.js
+++ dist/components/ClusterMarker/ClusterMarker.js
@@ -25,7 +25,7 @@ import Marker from '../Marker/Marker';
 var color = '#0000ff';
 var svg = window.btoa("\n  <svg fill=\"".concat(color, "\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 240 240\">\n    <circle cx=\"120\" cy=\"120\" opacity=\".6\" r=\"70\" />\n    <circle cx=\"120\" cy=\"120\" opacity=\".3\" r=\"90\" />\n    <circle cx=\"120\" cy=\"120\" opacity=\".2\" r=\"110\" />\n  </svg>"));
 var ClusterMarker = function (_a) {
-    var position = _a.position, count = _a.count, onClick = _a.onClick, options = __rest(_a, ["position", "count", "onClick"]);
+    var position = _a.position, count = _a.count, onClick = _a.onClick, onDoubleClick = _a.onDoubleClick, options = __rest(_a, ["position", "count", "onClick", "onDoubleClick"]);
     var clusterOptions = {
         icon: {
             url: "data:image/svg+xml;base64,".concat(svg),
@@ -38,7 +38,7 @@ var ClusterMarker = function (_a) {
         },
         zIndex: Number(google.maps.Marker.MAX_ZINDEX)
     };
-    return (React.createElement(Marker, __assign({ position: position, icon: clusterOptions.icon, label: clusterOptions.label, zIndex: clusterOptions.zIndex, onClick: onClick }, options)));
+    return (React.createElement(Marker, __assign({ position: position, icon: clusterOptions.icon, label: clusterOptions.label, zIndex: clusterOptions.zIndex, onClick: onClick, onDoubleClick: onDoubleClick }, options)));
 };
 export default ClusterMarker;
 //# sourceMappingURL=ClusterMarker.js.map
