63450c46daef5fe4558ab557aca427cfa29e4acc
alejandrol <alejandrol@mprest.com>
2022-06-09 17:07:28 +0300

    fix polygon opacity

--- dist/components/Polygon/Polygon.js
+++ dist/components/Polygon/Polygon.js
@@ -74,8 +74,8 @@ var Polygon = function (_a) {
                 strokeColor: '#3467ab',
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
-                fillColor: 'rgba(13, 56, 240, 0.4)',
-                fillOpacity: 0.3
+                fillColor: '#0000ff',
+                fillOpacity: 0.0
             });
             // @ts-ignore
             bermudaTriangle.setMap(map);
--- src/components/Polygon/Polygon.tsx
+++ src/components/Polygon/Polygon.tsx
@@ -77,8 +77,8 @@ const Polygon: React.FC<
         strokeColor: '#3467ab',
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: 'rgba(13, 56, 240, 0.4)',
-        fillOpacity: 0.3
+        fillColor: '#0000ff',
+        fillOpacity: 0.0
       });
 
       // @ts-ignore
