d6ba0d608a3b23bf0efa4a53e27883d4e787eb1a
alejandrol <alejandrol@mprest.com>
2022-06-16 11:12:49 +0300

    remove transparency

--- dist/components/Polygon/Polygon.js
+++ dist/components/Polygon/Polygon.js
@@ -75,7 +75,7 @@ var Polygon = function (_a) {
                 strokeOpacity: 0.8,
                 strokeWeight: 2,
                 fillColor: '#0000ff',
-                fillOpacity: 0.001
+                fillOpacity: 0.0
             });
             // @ts-ignore
             bermudaTriangle.setMap(map);
--- src/components/Polygon/Polygon.tsx
+++ src/components/Polygon/Polygon.tsx
@@ -78,7 +78,7 @@ const Polygon: React.FC<
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: '#0000ff',
-        fillOpacity: 0.001
+        fillOpacity: 0.0
       });
 
       // @ts-ignore
